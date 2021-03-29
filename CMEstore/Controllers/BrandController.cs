f3c66da374543e9f92b7d84635f52568730307e7
Андрей Брижак <andrewbrizhak@gmail.com>
2021-03-29 18:24:35 +0800

    Get all Brand

--- CMEstore/Controllers/BrandController.cs
+++ CMEstore/Controllers/BrandController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMEstore.Data;
+using CMEstore.Models;
 
 namespace CMEstore.Controllers
 {
     public class BrandController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BrandController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Brand> objList = _db.Brand;
+            return View(objList);
         }
     }
 }
