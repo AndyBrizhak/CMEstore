dd4b979ba268b780acd7eb9cf8e8b667d95e02fd
Андрей Брижак <andrewbrizhak@gmail.com>
2021-03-30 10:41:46 +0800

    Add Brand part 2.

--- CMEstore/Models/Brand.cs
+++ CMEstore/Models/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,11 @@ namespace CMEstore.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [DisplayName("Название Бренда")]
         public string Name { get; set; }
+
+        [DisplayName("Порядок вывода на экран")]
         public int DisplayOrder { get; set; }
     }
 }
