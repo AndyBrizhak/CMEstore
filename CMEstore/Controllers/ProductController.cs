7b9749258a1a6db717b220b301823c8484ae10e1
Андрей Брижак <andrewbrizhak@gmail.com>
2021-04-08 07:34:12 +0800

    Product List Action and View

---
+++ CMEstore/Controllers/ProductController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMEstore.Data;
+using CMEstore.Models;
+
+namespace CMEstore.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach (var obj in objList)
+            {
+                obj.Brand = _db.Brand.FirstOrDefault(u => u.Id == obj.BrandId);
+            };
+
+            return View(objList);
+        }
+
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Brand obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Brand.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Brand.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Brand obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Brand.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Brand.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Brand.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Brand.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+}
+}
