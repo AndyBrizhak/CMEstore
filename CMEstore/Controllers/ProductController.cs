340800be369b81564ab93f452a0204788a759df2
Андрей Брижак <andrewbrizhak@gmail.com>
2021-04-26 08:23:15 +0800

    Delete Product

--- CMEstore/Controllers/ProductController.cs
+++ CMEstore/Controllers/ProductController.cs
@@ -170,26 +170,40 @@ namespace CMEstore.Controllers
             {
                 return NotFound();
             }
-            var obj = _db.Brand.Find(id);
-            if (obj == null)
+
+            //Eager Loading options
+            Product product = _db.Product.Include(u => u.Brand).FirstOrDefault(u => u.Id == id);
+            
+            //Simple loading
+            //product.Brand = _db.Brand.Find(product.BrandId);
+
+            if (product == null)
             {
                 return NotFound();
             }
 
-            return View(obj);
+            return View(product);
         }
 
         //POST - DELETE
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeletePost(int? id)
         {
-            var obj = _db.Brand.Find(id);
+            var obj = _db.Product.Find(id);
             if (obj == null)
             {
                 return NotFound();
             }
-            _db.Brand.Remove(obj);
+
+            string upload = _webHostEnvironment.WebRootPath + WC.ImagePath;
+            var oldFile = Path.Combine(upload, obj.Image);
+            if (System.IO.File.Exists(oldFile))
+            {
+                System.IO.File.Delete(oldFile);
+            }
+
+            _db.Product.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
 
