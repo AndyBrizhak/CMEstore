5bdc477b309c9d85152763e0379217dd5729fcb1
Андрей Брижак <andrewbrizhak@gmail.com>
2021-05-04 08:40:50 +0800

    View Model for Home Page

--- CMEstore/Controllers/HomeController.cs
+++ CMEstore/Controllers/HomeController.cs
@@ -2,21 +2,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using CMEstore.Data;
+using CMEstore.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMEstore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(u => u.Brand),
+                Brands = _db.Brand
+            };
+           
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ CMEstore/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMEstore.Models.ViewModels
+{
+    public class HomeVM
+    {
+        /// <summary>
+        /// All Products from database 
+        /// </summary>
+        public IEnumerable<Product> Products { get; set; }
+
+        /// <summary>
+        /// All Brands from database
+        /// </summary>
+        public IEnumerable<Brand> Brands { get; set; }
+    }
+}
